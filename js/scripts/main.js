7e7ef1719d87082bfa76183f61b820f5dce28e6e
Gabriel Pereira de Almeida <pessoal.gabriel.pereira@gmail.com>
2024-08-07 19:37:13 -0300

    feat(section hero): add section hero

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,21 @@
+//Slide
+document.addEventListener('DOMContentLoaded', function() {
+    var slide_hero = new Swiper('.slide-hero', {
+      effect: 'fade',
+      pagination: {
+        el: '.slide-hero .swiper-pagination', 
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000, 
+        disableOnInteraction: false,
+      },
+      loop: true, 
+    });
+  });
+  
+
+//Modal
 const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
 const btnCloseModal = document.querySelector('.js-close-modal-details');
 
