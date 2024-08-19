d5175b03293b4ed459556815c1c3fb62514c1df4
Gabriel Pereira de Almeida <pessoal.gabriel.pereira@gmail.com>
2024-08-19 11:01:22 -0300

    feat(pokemon): Implement pokemon listing by type

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -119,7 +119,7 @@ function listingPokemons(urlApi) {
     })
 }
 
-listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0');
+listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=27&offset=0');
 
 
 
@@ -130,3 +130,60 @@ function openDetailsPokemon() {
 function closeDetailsPokemon() {
   document.documentElement.classList.remove('open-modal');
 }
+
+const areaTypes = document.getElementById('js-type-area');
+const areaTypesMobile = document.querySelector('.dropdown-select');
+
+axios({
+  method: 'GET',
+  url: 'https://pokeapi.co/api/v2/type'
+})
+.then(response => {
+  const { results } = response.data;
+
+  results.forEach((type, index) => {
+    
+    if (index < 18){
+
+      let itemType = document.createElement('li');
+      areaTypes.appendChild(itemType);
+  
+      let buttonType = document.createElement('button');
+      buttonType.classList = `type-filter ${type.name}`;
+      itemType.appendChild(buttonType);
+  
+      let iconType = document.createElement('div');
+      iconType.classList = "icon";
+      buttonType.appendChild(iconType);
+  
+      let srcType = document.createElement('img');
+      srcType.setAttribute('src', `img/icon-types/${type.name}.svg`);
+      iconType.appendChild(srcType);
+
+      let nameType = document.createElement('span');
+      nameType.textContent =(type.name);
+      buttonType.appendChild(nameType);
+
+      // Aqui é o preenchimento do select mobile dos tipos
+      let itemTypeMobile = document.createElement('li');
+      areaTypesMobile.appendChild(itemTypeMobile);
+
+      let buttonTypeMobile = document.createElement('button');
+      buttonTypeMobile.classList = `type-filter ${type.name}`
+      buttonTypeMobile.setAttribute('code-type', index + 1);
+      itemTypeMobile.appendChild(buttonTypeMobile);
+
+      let iconTypeMobile = document.createElement('div');
+      iconTypeMobile.classList = "icon";
+      buttonTypeMobile.appendChild(iconTypeMobile);
+
+      let srcTypeMobile = document.createElement('img');
+      srcTypeMobile.setAttribute('src', `img/icon-types/${type.name}.svg`);
+      iconTypeMobile.appendChild(srcTypeMobile);
+
+      let nameTypeMobile = document.createElement('span');
+      nameTypeMobile.textContent = (type.name);
+      buttonTypeMobile.appendChild(nameTypeMobile);      
+    }
+  })
+})
